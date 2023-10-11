8bbbaa230844137f0e20bae6333db7afa0a3d0e3
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2023-10-11 09:43:38 +0530

    Refactor(MAIN): add prisma imports

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import schema from '../prisma/schema.prisma';
+import x from '../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.debug(schema, x);
+}
 
 export const db = new PrismaClient({});
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,5 @@
-import { printSchema, lexicographicSortSchema } from 'graphql';
-import { writeFileSync } from 'fs';
 import { builder } from './builder';
 import './user';
 import './errors';
 
 export const schema = builder.toSchema();
-
-const schemaAsString = printSchema(lexicographicSortSchema(schema));
-writeFileSync(__dirname + '/schema.graphql', schemaAsString);
